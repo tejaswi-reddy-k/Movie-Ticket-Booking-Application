a3f091f0be1712e5db1e4f45c07f931c98ba7f6e
TEJASWI REDDY. KAMJULA <78067821+tejaswi-reddy-k@users.noreply.github.com>
2022-02-12 13:37:53 +0530

    Update Ticket.java

--- MovieTicketBooking/src/main/java/com/movie/app/entity/Ticket.java
+++ MovieTicketBooking/src/main/java/com/movie/app/entity/Ticket.java
@@ -1,22 +1,43 @@
 package com.movie.app.entity;
 
 import java.util.List;
+import java.util.Objects;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Ticket {
+	
+	public Ticket(int ticketId, List<String> seatNumber, @AssertTrue boolean ticketStatus,
+			@NotNull(message = "Screen Name Cannot be left Blank") String screenName) {
+		super();
+		this.ticketId = ticketId;
+		this.seatNumber = seatNumber;
+		this.ticketStatus = ticketStatus;
+		this.screenName = screenName;
+	}
+	public Ticket() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Id
 	private int ticketId;
-	private int noOfSeats;
-	@OneToMany
-	private List<Seat> seatNumber;
-	@ManyToOne
-	private Booking bookingRef;
+	
+	//private int noOfSeats;
+	@OneToOne(cascade=CascadeType.ALL)
+	private List<String> seatNumber;
+	//private Booking bookingRef;
+	@AssertTrue
 	private boolean ticketStatus;
+	@NotNull(message = "Screen Name Cannot be left Blank")
 	private String screenName;
 	public int getTicketId() {
 		return ticketId;
@@ -24,24 +45,24 @@ public class Ticket {
 	public void setTicketId(int ticketId) {
 		this.ticketId = ticketId;
 	}
-	public int getNoOfSeats() {
+	/*public int getNoOfSeats() {
 		return noOfSeats;
 	}
 	public void setNoOfSeats(int noOfSeats) {
 		this.noOfSeats = noOfSeats;
-	}
-	public List<Seat> getSeatNumber() {
+	}*/
+	public List<String> getSeatNumber() {
 		return seatNumber;
 	}
-	public void setSeatNumber(List<Seat> seatNumber) {
+	public void setSeatNumber(List<String> seatNumber) {
 		this.seatNumber = seatNumber;
 	}
-	public Booking getBookingRef() {
+	/*public Booking getBookingRef() {
 		return bookingRef;
 	}
 	public void setBookingRef(Booking bookingRef) {
 		this.bookingRef = bookingRef;
-	}
+	}*/
 	public boolean isTicketStatus() {
 		return ticketStatus;
 	}
@@ -55,9 +76,29 @@ public class Ticket {
 		this.screenName = screenName;
 	}
 	@Override
-	public String toString() {
-		return "Ticket [ticketId=" + ticketId + ", noOfSeats=" + noOfSeats + ", seatNumber=" + seatNumber
-				+ ", bookingRef=" + bookingRef + ", ticketStatus=" + ticketStatus + ", screenName=" + screenName + "]";
+	public int hashCode() {
+		return Objects.hash(screenName, seatNumber, ticketId, ticketStatus);
 	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ticket other = (Ticket) obj;
+		return Objects.equals(screenName, other.screenName) && Objects.equals(seatNumber, other.seatNumber)
+				&& ticketId == other.ticketId && ticketStatus == other.ticketStatus;
+	}
+	@Override
+	public String toString() {
+		return "Ticket [ticketId=" + ticketId + ", seatNumber=" + seatNumber + ", ticketStatus=" + ticketStatus
+				+ ", screenName=" + screenName + "]";
+	}
+	
+	
+	
+	
 
 }
