24c76e3b0e07405d1047e34db14e96af9caa09c7
TEJASWI REDDY. KAMJULA <78067821+tejaswi-reddy-k@users.noreply.github.com>
2022-02-12 17:56:06 +0530

    Update CustomEventRepository.java

--- MovieTicketBooking/src/main/java/com/movie/app/repository/CustomEventRepository.java
+++ MovieTicketBooking/src/main/java/com/movie/app/repository/CustomEventRepository.java
@@ -4,15 +4,19 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.movie.app.entity.Event;
 import com.movie.app.exceptions.EventNotFoundException;
 
+@Repository
 public interface CustomEventRepository {
 	
+	/*
 	public Event addEvent(Event event);
 	public Event removeEvent(int eventid) throws EventNotFoundException;
 	public Event updateEvent(Event event) throws EventNotFoundException;
-	/*
+	
 	public Event viewEvent(int eventid) throws EventNotFoundException;
 	public List<Event> viewEventList();
 	public List<Event> viewEventList(int eventid);
@@ -20,6 +24,11 @@ public interface CustomEventRepository {
 	public List<Event> viewEventList(LocalTime time);
 	*/
 	
-	//for service only
+	public List<Event> getEventsByType(String eventType);
+
+	public List<Event> getEventsByLanguage(String eventLanguage);
+	
+	
+
 
 }
