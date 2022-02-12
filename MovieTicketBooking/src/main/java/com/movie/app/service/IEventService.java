2b0c8da34bb9dd9569a4122e8ab892db77127c95
TEJASWI REDDY. KAMJULA <78067821+tejaswi-reddy-k@users.noreply.github.com>
2022-02-12 13:47:16 +0530

    Update IEventService.java

--- MovieTicketBooking/src/main/java/com/movie/app/service/IEventService.java
+++ MovieTicketBooking/src/main/java/com/movie/app/service/IEventService.java
@@ -1,18 +1,23 @@
 package com.movie.app.service;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import com.movie.app.entity.Event;
 import com.movie.app.exceptions.EventNotFoundException;
 
 public interface IEventService {
+	
 	public Event addEvent(Event event);
 	public Event removeEvent(int eventid) throws EventNotFoundException;
 	public Event updateEvent(Event event) throws EventNotFoundException;
-	public Event view(int eventid) throws EventNotFoundException;
+	public Event viewEvent(int eventid) throws EventNotFoundException;
 	public List<Event> viewEventList();
-	public List<Event> viewEventList(int theatreid);
+	public List<Event> viewEventList(int eventid);
 	public List<Event> viewEventList(LocalDate date);
+	public List<Event> viewEventList(LocalTime time);
+	
+	public Event searchEvent(int eventid) throws EventNotFoundException;
 
 }
