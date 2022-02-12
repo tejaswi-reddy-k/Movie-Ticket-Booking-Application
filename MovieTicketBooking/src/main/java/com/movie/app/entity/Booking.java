bec3487a4db58e9ea0c4e861b39f7e791fd25ed7
TEJASWI REDDY. KAMJULA <78067821+tejaswi-reddy-k@users.noreply.github.com>
2022-02-12 13:36:26 +0530

    Update Booking.java

--- MovieTicketBooking/src/main/java/com/movie/app/entity/Booking.java
+++ MovieTicketBooking/src/main/java/com/movie/app/entity/Booking.java
@@ -1,49 +1,70 @@
 package com.movie.app.entity;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.time.LocalTime;
 
-import javax.persistence.CascadeType;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.validation.constraints.Min;
 
 @Entity
 public class Booking {
+	
+	public Booking() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Booking(int bookingId, int businessId, LocalDate bookingDate, LocalTime bookingTime, int transactionId,
+			@Min(value = 500000, message = "Min rage of total booking cost is 500000.00") double totalCost,
+			@Min(value = 5000, message = "Min Seats booked is 5000") int noOfSeats, Ticket ticket) {
+		super();
+		this.bookingId = bookingId;
+		this.businessId = businessId;
+		this.bookingDate = bookingDate;
+		this.bookingTime = bookingTime;
+		this.transactionId = transactionId;
+		this.totalCost = totalCost;
+		this.noOfSeats = noOfSeats;
+		this.ticket = ticket;
+	}
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int bookingId;
-	private int movieId;
-	private int showId;
+	//private int movieId;
+	//private int eventId;
+	private int businessId;	
 	private LocalDate bookingDate;
-	@ManyToOne
-	private Show showRef;
+	private LocalTime bookingTime;
+	//private Show showRef;	
 	private int transactionId;
+	@Min(value = (long) 500000.00, message = "Min rage of total booking cost is 500000.00")
 	private double totalCost;
-	@OneToMany
-	private List<Seat> seatList;
-	@ManyToOne
+	//private List<Seat> seatList;
+	@Min(value = 5000, message = "Min Seats booked is 5000")
+	private int noOfSeats;
+	@Embedded
 	private Ticket ticket;
+	
 	public int getBookingId() {
 		return bookingId;
 	}
 	public void setBookingId(int bookingId) {
 		this.bookingId = bookingId;
 	}
-	public int getMovieId() {
+	/*public int getMovieId() {
 		return movieId;
 	}
 	public void setMovieId(int movieId) {
 		this.movieId = movieId;
+	}*/
+	public int getBusinessId() {
+		return businessId;
 	}
-	public int getShowId() {
-		return showId;
-	}
-	public void setShowId(int showId) {
-		this.showId = showId;
+	public void setBusinessId(int businessId) {
+		this.businessId = businessId;
 	}
 	public LocalDate getBookingDate() {
 		return bookingDate;
@@ -51,30 +72,46 @@ public class Booking {
 	public void setBookingDate(LocalDate bookingDate) {
 		this.bookingDate = bookingDate;
 	}
-	public Show getShowRef() {
+	
+	public LocalTime getBookingTime() {
+		return bookingTime;
+	}
+	public void setBookingTime(LocalTime bookingTime) {
+		this.bookingTime = bookingTime;
+	}
+	/*public Show getShowRef() {
 		return showRef;
 	}
 	public void setShowRef(Show showRef) {
 		this.showRef = showRef;
+	}*/
+	public int getNoOfSeats() {
+		return noOfSeats;
 	}
+	public void setNoOfSeats(int noOfSeats) {
+		this.noOfSeats = noOfSeats;
+	}
+	
 	public int getTransactionId() {
 		return transactionId;
 	}
 	public void setTransactionId(int transactionId) {
 		this.transactionId = transactionId;
 	}
+		
+		
 	public double getTotalCost() {
 		return totalCost;
 	}
 	public void setTotalCost(double totalCost) {
 		this.totalCost = totalCost;
 	}
-	public List<Seat> getSeatList() {
+	/*public List<Seat> getSeatList() {
 		return seatList;
 	}
 	public void setSeatList(List<Seat> seatList) {
 		this.seatList = seatList;
-	}
+	}*/
 	public Ticket getTicket() {
 		return ticket;
 	}
@@ -83,10 +120,12 @@ public class Booking {
 	}
 	@Override
 	public String toString() {
-		return "Booking [bookingId=" + bookingId + ", movieId=" + movieId + ", showId=" + showId + ", bookingDate="
-				+ bookingDate + ", showRef=" + showRef + ", transactionId=" + transactionId + ", totalCost=" + totalCost
-				+ ", seatList=" + seatList + ", ticket=" + ticket + "]";
+		return "Booking [bookingId=" + bookingId + ", businessId=" + businessId + ", bookingDate=" + bookingDate
+				+ ", bookingTime=" + bookingTime + ", transactionId=" + transactionId + ", totalCost=" + totalCost
+				+ ", noOfSeats=" + noOfSeats + ", ticket=" + ticket + "]";
 	}
+	
+	
 	
 	
 
