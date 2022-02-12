7f4b05d9a72f9fdea1098e248672e245b29574d6
TEJASWI REDDY. KAMJULA <78067821+tejaswi-reddy-k@users.noreply.github.com>
2022-02-12 13:46:26 +0530

    Update IBookingService.java

--- MovieTicketBooking/src/main/java/com/movie/app/service/IBookingService.java
+++ MovieTicketBooking/src/main/java/com/movie/app/service/IBookingService.java
@@ -1,6 +1,7 @@
 package com.movie.app.service;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import com.movie.app.entity.Booking;
@@ -11,11 +12,11 @@ public interface IBookingService {
 	public Booking addBooking(Booking booking);
 	public Booking updateBooking(Booking booking) throws BookingNotFoundException;
 	public Booking cancelBooking(int bookingid) throws BookingNotFoundException;
-	public List<Booking> showAllBookings(int movieid);
+	public List<Booking> showAllBookingsList(int businessid);
+	//public List<Booking> showAllBookings(int movieid);
+	//public List<Booking> showAllBookings(int eventid);
 	public List<Booking> showAllBookings(LocalDate bookingdate);
-	public List<Booking> showBookingList(int showid);
+	public List<Booking> showAllBookings(LocalTime bookingtime);
 	public double calculateTotalCost(int bookingid);
-	
-	public void diplay();
 
 }
