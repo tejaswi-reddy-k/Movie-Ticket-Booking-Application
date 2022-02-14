daa3d0fb27af7cb0440e7919ce2e4ba30a409305
TEJASWI REDDY. KAMJULA <78067821+tejaswi-reddy-k@users.noreply.github.com>
2022-02-14 11:36:53 +0530

    Update IBookingService.java

--- MovieTicketBooking/src/main/java/com/movie/app/service/IBookingService.java
+++ MovieTicketBooking/src/main/java/com/movie/app/service/IBookingService.java
@@ -1,21 +1,42 @@
 package com.movie.app.service;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
-import com.movie.app.entity.Booking;
-import com.movie.app.exceptions.BookingNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.movie.app.entity.Booking;
+import com.movie.app.entity.Event;
+import com.movie.app.entity.Ticket;
+import com.movie.app.exceptions.BookingNotFoundException;
+import com.movie.app.exceptions.EventNotFoundException;
+
+@Service
 public interface IBookingService {
 	
 	public Booking addBooking(Booking booking);
-	public Booking updateBooking(Booking booking) throws BookingNotFoundException;
+	public String updateBooking(Booking booking) throws BookingNotFoundException;
 	public Booking cancelBooking(int bookingid) throws BookingNotFoundException;
-	public List<Booking> showAllBookings(int movieid);
-	public List<Booking> showAllBookings(LocalDate bookingdate);
-	public List<Booking> showBookingList(int showid);
-	public double calculateTotalCost(int bookingid);
 	
-	public void diplay();
+	/*public List<Booking> showAllBookingsList(int businessid);
+	public List<Booking> showAllBookings(int movieid);
+	public List<Booking> showAllBookings(int eventid);*/
+	
+	/*public List<Booking> showAllBookings(LocalDate bookingdate);
+	public List<Booking> showAllBookings(LocalTime bookingtime);*/
+	
+	//public double calculateTotalCost(double totalcost);
+
+	
+	public Booking getBookingById(int bookingid);
+	public List<Booking> viewBookingList();
+	public boolean deleteBookingByID(int bookingid);
+	public boolean checkBookingById(int bookingid);
+	
+	public List<Ticket> viewTicketsByBookingId(int bookingid) throws BookingNotFoundException;
+	//public boolean addTicket(int bookingid,int ticketid);
+
 
 }
+
