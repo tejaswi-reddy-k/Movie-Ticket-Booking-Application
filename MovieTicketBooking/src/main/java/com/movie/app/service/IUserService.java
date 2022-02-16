4db6bb91128df956fecf5837adc8341e9e82d919
Charishma05 <96839021+Charishma05@users.noreply.github.com>
2022-02-16 16:14:50 +0530

    Update IUserService.java

--- MovieTicketBooking/src/main/java/com/movie/app/service/IUserService.java
+++ MovieTicketBooking/src/main/java/com/movie/app/service/IUserService.java
@@ -1,11 +1,16 @@
 package com.movie.app.service;
 
+import java.util.List;
+
 import com.movie.app.entity.User;
 
 public interface IUserService {
-	
-	public  User addNewUser(User user);
-	public User signIn(User user);
-	public User signOut(User user);
+	public List<User> getAllUsers();
+	public User getUserById(long user_id);
+	public User addUser(User newUser);
+	public User updateUser(User updatedUser,long user_id);
+	public void deleteUserById(long user_id);
+
 
 }
+
