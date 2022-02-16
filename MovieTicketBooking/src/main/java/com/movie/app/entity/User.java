92d8e133e021ffa5751b3dfbfc3665507c6278a9
Charishma05 <96839021+Charishma05@users.noreply.github.com>
2022-02-16 16:10:42 +0530

    Update User.java

--- MovieTicketBooking/src/main/java/com/movie/app/entity/User.java
+++ MovieTicketBooking/src/main/java/com/movie/app/entity/User.java
@@ -1,31 +1,47 @@
 package com.movie.app.entity;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "USER")
 public class User {
-	
-	@Id
-	private int userId;
-	private String password;
-	private String role;
-	
-	public int getUserId() {
-		return userId;
-	}
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getRole() {
-		return role;
-	}
-	public void setRole(String role) {
-		this.role = role;
-	}
+
+@Id
+private int userId;
+@NotNull(message="name should not be integers")
+private String userName;
+@NotNull(message="password should not be empty")
+private String password;
+@NotNull(message="please mention role of the user")
+private String role;
+
+public int getUserId() {
+return userId;
+}
+public void setUserId(int userId) {
+this.userId = userId;
+}
+public String getPassword() {
+return password;
+}
+public void setPassword(String password) {
+this.password = password;
+}
+public String getRole() {
+return role;
+}
+public void setRole(String role) {
+this.role = role;
+}
 
 }
