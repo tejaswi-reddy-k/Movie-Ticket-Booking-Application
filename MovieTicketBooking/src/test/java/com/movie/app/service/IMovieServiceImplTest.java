f9cb94278d47b2da3dee568a48b3dca92f9f1824
Rohit <180030661cse@gmail.com>
2022-02-17 12:38:51 +0530

    updated

--- MovieTicketBooking/src/test/java/com/movie/app/service/IMovieServiceImplTest.java
+++ MovieTicketBooking/src/test/java/com/movie/app/service/IMovieServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.movie.app.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
@@ -18,30 +19,36 @@ public class IMovieServiceImplTest {
 	@Test
 	void addMovie() {
 		when(movieservice.addMovie(new Movie())).thenReturn("Movie added successfully");
+		assertEquals(movieservice.addMovie(new Movie()), "Movie added successfully");
 	}
 
 	@Test
 	void removeMovie() throws MovieNotFoundException {
 		when((movieservice.removeMovie(1))).thenReturn("Movie removed Successfully");
+		assertEquals(movieservice.removeMovie(1), "Movie removed Successfully");
 	}
 
 	@Test
 	void viewMovie() throws MovieNotFoundException {
 		when((movieservice.viewMovie(198))).thenReturn(null);
+		assertEquals(movieservice.viewMovie(198), null);
 	}
 
 	@Test
 	void viewMoviesByGenre() throws MovieNotFoundException {
 		when((movieservice.viewMoviesByGenre("thriller"))).thenReturn(null);
+		assertEquals(movieservice.viewMoviesByGenre("thriller"), null);
 	}
 
 	@Test
 	void viewTheatresByMovieId() throws MovieNotFoundException {
 		when((movieservice.viewTheatresByMovieId(2))).thenReturn(null);
+		assertEquals(movieservice.viewTheatresByMovieId(2), null);
 	}
 
 	@Test
 	void viewMoviesList() throws MovieNotFoundException {
 		when((movieservice.viewMovieList())).thenReturn(null);
+		assertEquals(movieservice.viewMovieList(), null);
 	}
 }
--- MovieTicketBooking/src/test/java/com/movie/app/service/ITheatreServiceImplTest.java
+++ MovieTicketBooking/src/test/java/com/movie/app/service/ITheatreServiceImplTest.java
@@ -1,11 +1,10 @@
 package com.movie.app.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
-
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import com.movie.app.entity.Theatre;
 
 @SpringBootTest
@@ -17,21 +16,25 @@ public class ITheatreServiceImplTest {
 	@Test
 	void addTheatre() {
 		when((theatreservice.addTheatre(new Theatre()))).thenReturn("Theatre added Successfully");
+		assertEquals(theatreservice.addTheatre(new Theatre()), "Theatre added successfully");
 	}
 
 	@Test
 	void deleteTheatre() {
 		when((theatreservice.deleteTheatre(1))).thenReturn("Theatre removed Successfully");
+		assertEquals(theatreservice.deleteTheatre(1), "Theatre removed successfully");
 	}
 
 	@Test
 	void getTheatresBasedOnLocation() {
 		when((theatreservice.getTheatresBasedOnLocation("guntur"))).thenReturn(null);
+		assertEquals(theatreservice.getTheatresBasedOnLocation("guntur"), null);
 	}
 
 	@Test
 	void getTheatresBasedOnMovieAndLocation() {
 		when((theatreservice.getTheatresBasedOnMovieAndLocation("race", "guntur"))).thenReturn(null);
+		assertEquals(theatreservice.getTheatresBasedOnMovieAndLocation("race", "guntur"), null);
 	}
 
 }
